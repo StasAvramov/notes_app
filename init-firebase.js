fa48770841a57798e98239418c1aad1d7717f3ae
stas <stanislavsavramov@gmail.com>
2021-01-15 18:05:55 +0200

    firebase/init

---
+++ init-firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD816F_RsLNULSgNRV7BUmkKgJprQGeu-I",
+  authDomain: "notes-react-app-cd4a7.firebaseapp.com",
+  projectId: "notes-react-app-cd4a7",
+  storageBucket: "notes-react-app-cd4a7.appspot.com",
+  messagingSenderId: "560531965265",
+  appId: "1:560531965265:web:099ac5dba034152d8cbf70"
+};
+firebase.initializeApp(firebaseConfig);
